7694043fbb78f440023aa2fac2eb6eb3ba9ab06c
SuhJiseok <nastudiolab@gmail.com>
2023-04-21 12:55:18 +0900

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? `/${name}/` : '',
+  basePath: '/NEXT',
+  trailingSlash: true,// 빌드 시 폴더 구조 그대로 생성
+
 }
+
+
+
+//next.config.js
+const debug = process.env.NODE_ENV !== 'production'
+const name = '/NEXT'
+
+
 
 module.exports = nextConfig
